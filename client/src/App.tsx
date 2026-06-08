c2d0f689806e67b0298ac73e6cda50f3818f6e4a
dais2026 <info@dynamicaistrategies.com.au>
2026-06-09 06:57:08 +1000

    Add client/src/App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,33 @@
-/**
- * Main App Component
- */
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/NotFound";
+import { Route, Switch } from "wouter";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import Home from "./pages/Home";
 
-import React from "react";
-import { AuthProvider } from "./contexts/AuthContext";
-import Dashboard from "./components/Dashboard";
-
-export default function App() {
+function Router() {
+  // make sure to consider if you need authentication for certain routes
   return (
-    <AuthProvider>
-      <Dashboard />
-    </AuthProvider>
+    <Switch>
+      <Route path={"/"} component={Home} />
+      <Route path={"/404"} component={NotFound} />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <ThemeProvider defaultTheme="dark">
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
